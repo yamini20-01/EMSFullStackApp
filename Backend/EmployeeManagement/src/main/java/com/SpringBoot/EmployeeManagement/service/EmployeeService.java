c187a35a172aa76698d9d37019c202c6fea5a543
Yamini Dodda <108978064+yamini20-01@users.noreply.github.com>
2024-02-18 20:50:44 +0530

    Backend

---
+++ Backend/EmployeeManagement/src/main/java/com/SpringBoot/EmployeeManagement/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.SpringBoot.EmployeeManagement.service;
+
+import com.SpringBoot.EmployeeManagement.dto.EmployeeDto;
+
+import java.util.List;
+
+public interface EmployeeService {
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+    EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
+
+    EmployeeDto updateEmployee(Long employeeId,EmployeeDto updatedEmployee);
+
+    void deleteEmployee(Long employeeId);
+}
