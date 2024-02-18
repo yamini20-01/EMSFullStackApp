74b0b18d3fa51174d26c3408d22a4ee31effe38b
Yamini Dodda <108978064+yamini20-01@users.noreply.github.com>
2024-02-18 20:37:42 +0530

    backend

---
+++ Backend/EmployeeManagement/src/main/java/com/SpringBoot/EmployeeManagement/controller/EmployeeController.java
@@ -0,0 +1,50 @@
+package com.SpringBoot.EmployeeManagement.controller;
+
+import com.SpringBoot.EmployeeManagement.dto.EmployeeDto;
+import com.SpringBoot.EmployeeManagement.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin("*")
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
+        EmployeeDto savedEmployee=employeeService.createEmployee(employeeDto);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long employeeId){
+       EmployeeDto employeeDto=employeeService.getEmployeeById(employeeId);
+        return ResponseEntity.ok(employeeDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EmployeeDto>> getAllEmployees(){
+        List<EmployeeDto> employees=employeeService.getAllEmployees();
+        return ResponseEntity.ok(employees);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable("id") Long employeeId,@RequestBody EmployeeDto updatedEmployee){
+       EmployeeDto employeeDto= employeeService.updateEmployee(employeeId,updatedEmployee);
+       return ResponseEntity.ok(employeeDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long employeeId){
+        employeeService.deleteEmployee(employeeId);
+        return ResponseEntity.ok("Employee deleted successfully");
+    }
+
+}
